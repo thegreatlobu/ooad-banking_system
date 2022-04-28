146c4bb304de22c9502cd2165440b5ab85a6879e
Divyansh Raina <divicoolman729@gmail.com>
2022-04-28 09:44:48 +0530

    Updated on 12:31 27/04/22

--- src/main/java/com/project/project/TransactionController.java
+++ src/main/java/com/project/project/TransactionController.java
@@ -2,7 +2,7 @@ package com.project.project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
